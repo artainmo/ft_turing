ea097e94e3a983b893266cbc858ffd0751e20696
Arthur Tainmont <arthurtainmont@Arthurs-Mac-mini.local>
2023-03-29 20:42:21 +0200

    verify command line arguments

--- src/main.ml
+++ src/main.ml
@@ -1,1 +1,18 @@
-Printf.fprintf stdout "Hello world"
+if Array.length Sys.argv < 2 || Array.length Sys.argv > 3 then begin
+    Printf.printf "Wrong number of command line arguments. Use '-h' for help.\n" ; 
+    exit 0
+end else if Array.length Sys.argv = 2 && (Sys.argv.(1) = "--help" || Sys.argv.(1) = "-h") then begin
+    Printf.printf "usage: ft_turing [-h] jsonfile input\n";
+    Printf.printf "\n";
+    Printf.printf "positional arguments:\n";
+    Printf.printf "  jsonfile            json description of the machine\n";
+    Printf.printf "\n";
+    Printf.printf "  input               input of the machine\n";
+    Printf.printf "\n";
+    Printf.printf "optional arguments:\n";
+    Printf.printf "  -h, --help          show this help message and exit\n";
+    exit 0
+end else if Array.length Sys.argv = 2 then begin
+    Printf.printf "Wrong argument. Use '-h' for help.\n" ; 
+    exit 0
+end
