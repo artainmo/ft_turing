cd9944bdaec52ab2ed9a3ce110e2a1553ad63c9e
Arthur Tainmont <arthurtainmont@Arthurs-Mac-mini.local>
2023-03-30 23:59:06 +0200

    improved parsing

--- src/main.ml
+++ src/main.ml
@@ -38,31 +38,60 @@ let parse_json json label conversion_function =
         | _ -> Printf.printf "Error parsing machine %s from '%s'" label Sys.argv.(1) ; exit 0
 
 let name = parse_json json "name" Yojson.Basic.Util.to_string
-let alphabet = parse_json json "alphabet" Yojson.Basic.Util.to_list
+let _alphabet = parse_json json "alphabet" Yojson.Basic.Util.to_list
+let alphabet = List.map Yojson.Basic.to_string _alphabet
 let blank = parse_json json "blank" Yojson.Basic.Util.to_string
-let states = parse_json json "states" Yojson.Basic.Util.to_list
+let _states = parse_json json "states" Yojson.Basic.Util.to_list
+let states = List.map Yojson.Basic.to_string _states
 let initial = parse_json json "initial" Yojson.Basic.Util.to_string
-let finals = parse_json json "finals" Yojson.Basic.Util.to_list
-let transitions = parse_json json "transitions" Yojson.Basic.Util.to_assoc
+let _finals = parse_json json "finals" Yojson.Basic.Util.to_list
+let finals = List.map Yojson.Basic.to_string _finals
+
+type rule = { read: string; to_state: string; write: string; action: string }
+let __transitions = parse_json json "transitions" Yojson.Basic.Util.to_assoc
+let _transitions = List.map (fun (state,rules) -> (state, rules |> Yojson.Basic.Util.to_list)) __transitions 
+let transitions = List.map(fun (state,rules) -> (state, List.map(fun rule -> {
+                read=parse_json rule "read" Yojson.Basic.Util.to_string;
+                to_state=parse_json rule "to_state" Yojson.Basic.Util.to_string;
+                write=parse_json rule "write" Yojson.Basic.Util.to_string;
+                action=parse_json rule "action" Yojson.Basic.Util.to_string
+            }) rules)) _transitions
 
 let () = Printf.printf "input: %s\n" input
 let () = Printf.printf "name: %s\n" name
 let () = Printf.printf "alphabet:\n"
-let () = List.iter (fun x -> print_endline ("  " ^ Yojson.Basic.to_string x)) alphabet
+let () = List.iter (fun x -> print_endline ("  " ^ x)) alphabet
 let () = Printf.printf "blank: %s\n" blank
 let () = Printf.printf "states:\n"
-let () = List.iter (fun x -> print_endline ("  " ^ Yojson.Basic.to_string x)) states
+let () = List.iter (fun x -> print_endline ("  " ^ x)) states
 let () = Printf.printf "initial: %s\n" initial
 let () = Printf.printf "finals:\n"
-let () = List.iter (fun x -> print_endline ("  " ^ Yojson.Basic.to_string x)) finals
+let () = List.iter (fun x -> print_endline ("  " ^ x)) finals
 let () = Printf.printf "transitions:\n"
 let () = List.iter (fun (state, rules) -> 
     Printf.printf "  state %s:\n" state;
     List.iter (fun rule ->
-        let read = parse_json rule "read" Yojson.Basic.Util.to_string in
-        let to_state = parse_json rule "to_state" Yojson.Basic.Util.to_string in
-        let write = parse_json rule "write" Yojson.Basic.Util.to_string in
-        let action = parse_json rule "action" Yojson.Basic.Util.to_string in
+        let read = rule.read in
+        let to_state = rule.to_state in
+        let write = rule.write in
+        let action = rule.action in
         Printf.printf "    read: %s, to_state: %s, write: %s, action: %s\n" read to_state write action
-    ) (rules |> Yojson.Basic.Util.to_list)
+    ) rules
 ) transitions
+(*
+let verify_states_in_transitions states transitions = 
+    List.for_all (fun state ->
+        List.exists (fun (state_, rules) ->
+            (*if state = state_ then true else false*)
+            Printf.printf "  state %s:\n" state;
+            Printf.printf "  state_ %s:\n" state_;
+            true
+        ) transitions
+    ) states
+
+let () = 
+    try
+        verify_states_in_transitions states transitions
+    with
+        | Not_found -> Printf.printf "A state has not been described as a transition."
+ *)
